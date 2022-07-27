35065d10f396b8277bfd4c80b886d39d44bcbe6f
Shwe Kyee <shwekyee.football@gmail.com>
2022-07-27 15:07:09 +0630

    Hotelwithadminpanel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <NavBar></NavBar>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '../components/NavBar'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
